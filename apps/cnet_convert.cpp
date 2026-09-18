ad8ab14e411252be81b6ef37c0559c66780bcc7d
Kelvin Rodriguez <krodriguez@usgs.gov>
2026-09-18 16:32:49 -0700

    fixed license

--- apps/cnet_convert.cpp
+++ apps/cnet_convert.cpp
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2024 USGS Astrogeology Science Center
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 // cnet_convert: convert control networks between the protobuf .net, Parquet, and
 // StarDS formats. Format is inferred from each path's extension. .net and
 // .parquet may be /vsi paths; .stards is a local-filesystem columnar store.
